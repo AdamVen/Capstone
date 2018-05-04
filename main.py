83b86004aef60237ac44da23e110a9c6430c49d1
Adam <adam.vengroff@gmail.com>
2018-05-04 15:16:14 -0700

    Fix plotting Refactor code

--- main.py
+++ main.py
@@ -17,9 +17,7 @@ import openpyxl
 
 # Custom-written module imports
 from DualOutput import DualOutput
-from ParameterGraph import ParameterGraph
 from SheetsThread import SheetsThread
-#from GUIThread import GUIThread
 
 # Google Project Api Imports
 import gspread
@@ -34,6 +32,9 @@ import time
 
 # Time stamping
 import datetime
+
+# Data simulation
+import random
 
 # GUI
 #import tkinter as tk
@@ -51,72 +52,6 @@ import matplotlib.animation as animation
 
 # For speed improvements
 from collections import deque
-
-def animate(i):
-    lineCurGood.set_data(range(len(curDataGood)), curDataGood)
-    lineCurBad.set_data(range(len(curDataGood)), curDataBad)
-    lineDistGood.set_data(range(len(curDataGood)), distDataGood)
-    lineDistBad.set_data(range(len(curDataGood)), distDataBad)
-    lineAngGood.set_data(range(len(curDataGood)), angDataGood)
-    lineAngBad.set_data(range(len(curDataGood)), angDataBad)
-    lineAccGood.set_data(range(len(curDataGood)), accDataGood)
-    lineAccBad.set_data(range(len(curDataGood)), accDataBad)
-    return lineCurGood, lineCurBad, lineDistGood, lineDistBad,\
-           lineAngGood, lineAngBad, lineAccGood, lineAccBad
-
-def animateinit():
-    return lineCurGood, lineCurBad, lineDistGood, lineDistBad,\
-           lineAngGood, lineAngBad, lineAccGood, lineAccBad
-
-def putData(data, param):
-    if param == 0:
-        if curRange[0] < data < curRange[1]:
-            curDataGood.append(data)
-            curDataBad.append(None)
-        else:
-            curDataGood.append(None)
-            curDataBad.append(data)
-
-        curDataGood.popleft()
-        curDataBad.popleft()
-
-    elif param == 1:
-        if distRange[0] < data < distRange[1]:
-            distDataGood.append(data)
-            distDataBad.append(None)
-        else:
-            distDataGood.append(None)
-            distDataBad.append(data)
-
-        distDataGood.popleft()
-        distDataBad.popleft()
-        print(distDataGood)
-
-    elif param == 2:
-        if angRange[0] < data < angRange[1]:
-            angDataGood.append(data)
-            angDataBad.append(None)
-        else:
-            angDataGood.append(None)
-            angDataBad.append(data)
-        angDataGood.popleft()
-        angDataBad.popleft()
-
-    elif param == 3:
-        if accRange[0] < data < accRange[1]:
-            accDataGood.append(data)
-            accDataBad.append(None)
-        else:
-            accDataGood.append(None)
-            accDataBad.append(data)
-        accDataGood.popleft()
-        accDataBad.popleft()
-
-def updateFrame():
-    LABEL.config(font=("Courier", 55))
-    LABEL.pack()
-    f.pack()
-    ROOT.update()
 
 # Define x limit
 xLim = 50
@@ -166,11 +101,6 @@ thickness = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
 metalIndex = int(sheet.cell(3, 3).value)
 transferIndex = int(sheet.cell(4, 3).value)
 thicknessIndex = int(sheet.cell(5, 3).value)
-
-# For iterating through Google Sheets cells
-cellIndex = 0
-index = 0
-indexOffset = 0
 
 # Set-up USB
 ser = serial.Serial('/dev/ttyUSB0', BAUD_RATE)
@@ -233,6 +163,202 @@ var = StringVar()
 var.set("test")
 LABEL = Label(f, textvariable = var)
 
+
+def animate(i):
+    lineCurGood.set_data(range(len(curDataGood)), curDataGood)
+    lineCurBad.set_data(range(len(curDataGood)), curDataBad)
+    lineDistGood.set_data(range(len(curDataGood)), distDataGood)
+    lineDistBad.set_data(range(len(curDataGood)), distDataBad)
+    lineAngGood.set_data(range(len(curDataGood)), angDataGood)
+    lineAngBad.set_data(range(len(curDataGood)), angDataBad)
+    lineAccGood.set_data(range(len(curDataGood)), accDataGood)
+    lineAccBad.set_data(range(len(curDataGood)), accDataBad)
+    return lineCurGood, lineCurBad, lineDistGood, lineDistBad,\
+           lineAngGood, lineAngBad, lineAccGood, lineAccBad
+
+def animateinit():
+    return lineCurGood, lineCurBad, lineDistGood, lineDistBad,\
+           lineAngGood, lineAngBad, lineAccGood, lineAccBad
+
+def putData(data, param):
+    if param == 0:
+        if curRange[0] < data < curRange[1]:
+            curDataGood.append(data)
+            curDataBad.append(None)
+        else:
+            curDataGood.append(None)
+            curDataBad.append(data)
+
+        curDataGood.popleft()
+        curDataBad.popleft()
+
+    elif param == 1:
+        if distRange[0] < data < distRange[1]:
+            distDataGood.append(data)
+            distDataBad.append(None)
+        else:
+            distDataGood.append(None)
+            distDataBad.append(data)
+
+        distDataGood.popleft()
+        distDataBad.popleft()
+
+    elif param == 2:
+        if angRange[0] < data < angRange[1]:
+            angDataGood.append(data)
+            angDataBad.append(None)
+        else:
+            angDataGood.append(None)
+            angDataBad.append(data)
+        angDataGood.popleft()
+        angDataBad.popleft()
+
+    elif param == 3:
+        if accRange[0] < data < accRange[1]:
+            accDataGood.append(data)
+            accDataBad.append(None)
+        else:
+            accDataGood.append(None)
+            accDataBad.append(data)
+        accDataGood.popleft()
+        accDataBad.popleft()
+
+def updateFrame():
+    LABEL.config(font=("Courier", 55))
+    LABEL.pack()
+    f.pack()
+    ROOT.update()
+
+def RunThread():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1920, 1080)
+        camera.framerate = 30
+        camera.brightness = 55
+        camera.annotate_text_size = 16
+        i = 0
+        param = 0
+
+        currentTime = time.time()
+
+        streamFlag = int(sheet.cell(6, 3).value)
+
+        try:
+            tsString = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+            if ((streamFlag == 1) or (streamFlag == 3)):
+                server_socket = socket.socket()
+
+                try:
+                    server_socket.bind(('0.0.0.0', 8000))
+                    print('Waiting on connection via port 8000')
+                except:
+                    server_socket.bind(('0.0.0.0', 8001))
+                    print('Waiting on connection via port 8001')
+
+                server_socket.listen(0)
+                connection = server_socket.accept()[0].makefile('wb')
+
+                if (streamFlag == 1):
+                    print('Stream Starting')
+                    camera.start_recording(connection, format='h264')
+                else:
+                    print('Stream and Recording Starting')
+                    customOutput = DualOutput('/home/pi/Downloads/' + tsString + '.h264', connection)
+                    camera.start_recording(customOutput, format='h264')
+
+            elif (streamFlag == 2):
+                print('Recording Starting')
+                camera.start_recording('/home/pi/Downloads/' + tsString + '.h264')
+
+            timeout = time.time() + SESSION_LENGTH
+
+            prevTime = time.time()
+
+            # For iterating through Google Sheets cells
+            cellIndex = 0
+            index = 0
+            indexOffset = 0
+
+            while time.time() < timeout:
+                data = str(ser.readline())
+
+                try:
+                    rawData, measurement = data.split(":")
+                    measurementValue = measurement.split("\\", 1)[0]
+                except:
+                    rawData = ""
+                    print('Serial Read Error')
+
+                if "C" in rawData:
+                    param = 0
+                    measurementValue = 200 + random.uniform(-3, 3)
+                    current.append(round(float(measurementValue), 3))
+
+                elif "D" in rawData:
+                    param = 1
+                    measurementValue = 11 + random.uniform(-2, 2)
+                    distance.append(measurementValue)
+
+                elif "angLR" in rawData:
+                    angle.append(measurementValue)
+                    param = 2
+                    if float(angle[-1]) > 75:
+                        i = 0
+                    elif float(angle[-1]) < 55:
+                        i = 2
+                    else:
+                        i = 1
+
+                elif "accLR" in rawData:
+                    accLR.append(round(float(measurementValue), 3))
+                    param = 3
+
+                elif "accFB" in rawData:
+                    accFB.append(round(float(measurementValue), 3))
+                    param = 4
+
+                elif "angFB" in rawData:
+                    param = 4
+
+                parsedData = measurementValue
+
+                index = index + 1
+                timestamp.append(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+
+                currentStr = 'Current: ' + str(currentLow) + ' < ' + str(current[-1])[:3] + ' < ' + str(currentHigh) + '\n'
+                distanceStr = 'Distance: ' + str(distLow) + ' < ' + str(distance[-1])[:5] + ' < ' + str(distHigh) + '\n'
+                angleStr = 'Angle (' + str(angleType[i]) + '): ' + str(angleLow[i]) + ' < ' + str(angle[-1])[:3] + ' < ' + str(angleHigh[i]) + '\n'
+                accStr = 'LR Acc: ' + str(accLR[-1])[:3] + ' FB Acc: ' + str(accFB[-1])[:3]
+                outStr = currentStr + distanceStr + angleStr + accStr
+                camera.annotate_text = outStr
+
+                if displayMode == 1:
+                    var.set(outStr)
+                    updateFrame()
+                elif displayMode == 2:
+                    prevTime = time.time()
+                    putData(float(parsedData), param)
+
+                # Call thread to write to Google sheets and update GUI every UPDATE_FREQ samples
+                if (index % UPDATE_FREQ == 0):
+                    newSheetThread = threading.Thread(target=SheetsThread,
+                                                      args=(indexOffset, UPDATE_FREQ, parameterList, sheet,))
+                    newSheetThread.start()
+                    indexOffset = index
+
+            camera.stop_recording()
+
+        finally:
+            if (streamFlag == 1 or streamFlag == 3):
+                connection.close()
+                server_socket.close()
+                print('Stream Ended')
+            else:
+                print('Recording Complete')
+
+        f.destroy()
+        ROOT.destroy()
+
 fig = Figure(figsize = (12, 7), dpi = 100)
 
 curPlot = fig.add_subplot(221)
@@ -276,139 +402,12 @@ Frame.pack(f)
 
 ani = animation.FuncAnimation(fig, animate, interval=40, init_func = animateinit, blit = True)
 ROOT.config(cursor="none")
-print('entering main loop')
+print('Activating GUI')
+
+newRunThread = threading.Thread(target=RunThread, args=())
+newRunThread.start()
+
 ROOT.mainloop()
 
-with picamera.PiCamera() as camera:
-    camera.resolution = (1920, 1080)
-    camera.framerate = 30
-    camera.brightness = 55
-    camera.annotate_text_size = 16
-    i = 0
-    param = 0
-
-    currentTime = time.time()
-
-    streamFlag = int(sheet.cell(6, 3).value)
-
-    try:
-        tsString = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-
-        if ((streamFlag == 1) or (streamFlag == 3)):
-            server_socket = socket.socket()
-
-            try:
-                server_socket.bind(('0.0.0.0', 8000))
-                print('Waiting on connection via port 8000')
-            except:
-                server_socket.bind(('0.0.0.0', 8001))
-                print('Waiting on connection via port 8001')
-
-            server_socket.listen(0)
-            connection = server_socket.accept()[0].makefile('wb')
-
-            if (streamFlag == 1):
-                print('Stream Starting')
-                camera.start_recording(connection, format='h264')
-            else:
-                print('Stream and Recording Starting')
-                customOutput = DualOutput('/home/pi/Downloads/' + tsString + '.h264', connection)
-                camera.start_recording(customOutput, format='h264')
-
-        elif (streamFlag == 2):
-            print('Recording Starting')
-            camera.start_recording('/home/pi/Downloads/' + tsString + '.h264')
-
-        timeout = time.time() + SESSION_LENGTH
-
-        #LOOP_ACTIVE = True
 
 
-        prevTime = time.time()
-
-
-        while time.time() < timeout:
-            data = str(ser.readline())
-
-            try:
-                rawData, measurement = data.split(":")
-                measurementValue = measurement.split("\\", 1)[0]
-            except:
-                rawData = ""
-                print('Serial Read Error')
-
-            if "C" in rawData:
-                param = 0
-                measurementValue = 200 + random.uniform(-3, 3)
-                current.append(round(float(measurementValue), 3))
-
-            elif "D" in rawData:
-                param = 1
-                measurementValue = 11 + random.uniform(-2, 2)
-                distance.append(measurementValue)
-
-            elif "angLR" in rawData:
-                angle.append(measurementValue)
-                param = 2
-                if float(angle[-1]) > 75:
-                    i = 0
-                elif float(angle[-1]) < 55:
-                    i = 2
-                else:
-                    i = 1
-
-            elif "accLR" in rawData:
-                accLR.append(round(float(measurementValue), 3))
-                param = 3
-
-            elif "accFB" in rawData:
-                accFB.append(round(float(measurementValue), 3))
-                param = 4
-
-            elif "angFB" in rawData:
-                param = 4
-
-            parsedData = measurementValue
-
-            index = index + 1
-            timestamp.append(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
-
-            currentStr = 'Current: ' + str(currentLow) + ' < ' + str(current[-1])[:3] + ' < ' + str(currentHigh) + '\n'
-            distanceStr = 'Distance: ' + str(distLow) + ' < ' + str(distance[-1])[:5] + ' < ' + str(distHigh) + '\n'
-            angleStr = 'Angle (' + str(angleType[i]) + '): ' + str(angleLow[i]) + ' < ' + str(angle[-1])[:3] + ' < ' + str(angleHigh[i]) + '\n'
-            accStr = 'LR Acc: ' + str(accLR[-1])[:3] + ' FB Acc: ' + str(accFB[-1])[:3]
-            outStr = currentStr + distanceStr + angleStr + accStr
-            camera.annotate_text = outStr
-
-            if displayMode == 1:
-                var.set(outStr)
-                updateFrame()
-            elif displayMode == 2:
-                #newGUIThread = threading.Thread(target=GUIThread, args=(parsedData, param, paramGraph))
-                #newGUIThread.start()
-                print(str(time.time() - prevTime))
-                prevTime = time.time()
-                putData(float(parsedData), param)
-                #paramGraph.drawGraph()
-                #f.pack()
-                ROOT.update()
-                ROOT.update_idletasks()
-
-            # Call thread to write to Google sheets and update GUI every UPDATE_FREQ samples
-            if (index % UPDATE_FREQ == 0):
-                newSheetThread = threading.Thread(target = SheetsThread, args = (indexOffset, UPDATE_FREQ, parameterList, sheet, ))
-                newSheetThread.start()
-                indexOffset = index
-
-        camera.stop_recording()
-
-    finally:
-        if(streamFlag == 1 or streamFlag == 3):
-            connection.close()
-            server_socket.close()
-            print('Stream Ended')
-        else:
-            print('Recording Complete')
-
-    f.destroy()
-    ROOT.destroy()
